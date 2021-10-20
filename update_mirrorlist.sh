3950193e1b862bc28254b6469f4623baff55f8bd
jgrf77 <justinfletcher90@gmail.com>
2021-10-20 13:16:33 +1300

    upload update_mirrorlist.sh

---
+++ update_mirrorlist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Look up country iso-code with ifconfig.co and set as variable iso
+iso=$(curl -4 ifconfig.co/country-iso)
+
+#create a backup of the mirrorlist
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+
+#Install reflector and rsync
+pacman -S reflector rsync --noconfirm --needed
+
+#scan the 20 most recently updated mirrors from country ($iso) and update mirrorlist to contain the 5 fastest sorted rate (speed)
+reflector -c $iso -f 5 -l 20 --verbose --sort rate --save /etc/pacman.d/mirrorlist
+
