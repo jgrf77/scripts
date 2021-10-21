c97f39cc5d5241b4be551fb1735ba969a1ba61d5
jgrf77 <justinfletcher90@gmail.com>
2021-10-21 18:42:31 +1300

    add interaction for formatting disk

--- test.sh
+++ test.sh
@@ -36,32 +36,15 @@ lsblk
 echo "Please enter disk to work on: (example /dev/sda)"
 read DISK
 
-#Prompt user to confirm with yes
+#Prompt user response and store as variable formatdisk
 echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK"
 read -p "are you sure you want to continue (Y/N):" formatdisk
+
+#If $formatdisk is yes then run disc format commands
 case $formatdisk in
-
 y|Y|yes|Yes|YES)
-echo "--------------------------------------"
-echo -e "\nFormatting disk...\n$HR"
-echo "--------------------------------------"
-
-#FORMAT DISK
-echo -e "\nInstalling prereqs...\n$HR"
-pacman -S --noconfirm gptfdisk btrfs-progs
-
-echo "-------------------------------------------------"
-echo "-------select your disk to format----------------"
-echo "-------------------------------------------------"
-lsblk
-echo "Please enter disk to work on: (example /dev/sda)"
-read DISK
-echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK"
-read -p "are you sure you want to continue (Y/N):" formatdisk
-case $formatdisk in
-
-y|Y|yes|Yes|YES)
-echo "--------------------------------------"
-echo -e "\nFormatting disk...\n$HR"
-echo "--------------------------------------"
-
+cfdisk
+;;
+*) echo "you have declined disc formatting"
+;;
+esac
