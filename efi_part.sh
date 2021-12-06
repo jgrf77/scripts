9035860a688ccff11ef751690d42153f1264fb59
jgrf77 <justinfletcher90@gmail.com>
2021-12-07 11:56:36 +1300

    change HD=sda back to HD=/dev/sda and update references

--- efi_part.sh
+++ efi_part.sh
@@ -2,7 +2,7 @@
 ##################################################
 ########## Hard Disk Partitioning Variable########
 # ANTENTION, this script erases ALL YOU HD DATA (specified bt $HD)
-HD=sda #CHANGED FROM /dev/sda
+HD=/dev/sda
 # Boot Partition Size: /boot
 BOOT_SIZE=200
 # Root Partition Size: /
@@ -30,18 +30,18 @@ HOME_START=$ROOT_END
 #####################################################
 
 
-#  dd bs=512 if=/dev/zero of=/dev/"${HD}" count=8192
-#  dd bs=512 if=/dev/zero of=/dev/"${HD}" count=8192 seek=$((`blockdev --getsz /dev/"${HD}"` - 8192))
+#  dd bs=512 if=/dev/zero of="${HD}" count=8192
+#  dd bs=512 if=/dev/zero of="${HD}" count=8192 seek=$((`blockdev --getsz "${HD}"` - 8192))
 
   #Clear clear partition data and set to GPT disk with 2048 alignment
   sgdisk -Z ${HD} # zap all on disk
   sgdisk -a 2048 -o ${HD} # new gpt disk 2048 alignment
   
   #Make partitions
-  sgdisk -n 0:0:+650MiB -t 0:ef00 -c 0:efi /dev/"${HD}"
-  sgdisk -n 0:0:+"${SWAP_SIZE}MiB" -t 0:8200 -c 0:swap /dev/"${HD}"
-  sgdisk -n 0:0:+"${ROOT_SIZE}MiB" -t 0:8303 -c 0:root /dev/"${HD}"
-  sgdisk -n 0:0:0 -t 0:8302 -c 0:home /dev/"${HD}"
+  sgdisk -n 0:0:+650MiB -t 0:ef00 -c 0:efi "${HD}"
+  sgdisk -n 0:0:+"${SWAP_SIZE}MiB" -t 0:8200 -c 0:swap "${HD}"
+  sgdisk -n 0:0:+"${ROOT_SIZE}MiB" -t 0:8303 -c 0:root "${HD}"
+  sgdisk -n 0:0:0 -t 0:8302 -c 0:home "${HD}"
   clear
   echo -e "\n"
   echo "Partitions created..."
@@ -50,10 +50,10 @@ HOME_START=$ROOT_END
   
   #Format partitions
   clear
-  mkswap -L swap /dev/"${HD}"\2
-  mkfs.fat -F32 /dev/"${HD}"\1
-  mkfs.ext4 -L root /dev/"${HD}"\3
-  mkfs.ext4 -L home /dev/"${HD}"\4
+  mkswap -L swap "${HD}"\2
+  mkfs.fat -F32 "${HD}"\1
+  mkfs.ext4 -L root "${HD}"\3
+  mkfs.ext4 -L home "${HD}"\4
   clear
   echo -e "\n"
   echo "Partitions formatted..."
@@ -62,12 +62,12 @@ HOME_START=$ROOT_END
   
   #Mount partitions
   clear
-  mount /dev/"${TRGTDRV}"\3 /mnt
+  mount "${HD}"\3 /mnt
   mkdir /mnt/efi
-  mount /dev/"${TRGTDRV}"\1 /mnt/efi
+  mount "${HD}"\1 /mnt/efi
   mkdir /mnt/home
-  mount /dev/"${TRGTDRV}"\4 /mnt/home
-  swapon /dev/"${TRGTDRV}"\2
+  mount "${HD}"\4 /mnt/home
+  swapon "${HD}"\2
   clear
   echo -e "\n"
   echo "Mounted partitions..."
