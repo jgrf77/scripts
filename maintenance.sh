6a6d1d6b55caf75957df99b19951b8270ba08a07
jgrf77 <justinfletcher90@gmail.com>
2022-01-22 21:18:04 +1300

    update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -8,10 +8,10 @@ function mirrors {
 	iso=$(curl -4 ifconfig.co/country-iso)
 
 	#create a backup of the mirrorlist
-	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+	sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
 	#scan the 20 most recently updated mirrors from country ($iso) and update mirrorlist to contain the 5 fastest sorted rate (speed)
-	reflector -c $iso -f 5 -l 20 --verbose --sort rate --save /etc/pacman.d/mirrorlist
+	sudo reflector -c $iso -f 5 -l 20 --verbose --sort rate --save /etc/pacman.d/mirrorlist
 }
 
 function update {
